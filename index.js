285e63d0bf67ff4e7e40a188791356125edb56f9
ThePcda <124522021+ThePcda@users.noreply.github.com>
2024-09-03 10:23:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 let myVar = 45;
+
+const mySpan = document.getElementById("mySpan");
+
+mySpan.addEventListener("click", mySpanFunction);
+
+function mySpanFunction() {
+  mySpan.innerHTML = "Clicked!";
+}
